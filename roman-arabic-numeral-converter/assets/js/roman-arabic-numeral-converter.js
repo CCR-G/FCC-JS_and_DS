91ffbd64c2e47a95ade8fd611e1955553ecad617
CCR <->
2020-07-06 20:33:10 +0200

    Commenting code
    Used JSdoc.app
    Created a new isInvalidRomanNum() function

--- roman-arabic-numeral-converter/assets/js/roman-arabic-numeral-converter.js
+++ roman-arabic-numeral-converter/assets/js/roman-arabic-numeral-converter.js
@@ -8,15 +8,18 @@ const romanTable = ["I", "V", "X", "L", "C", "D", "M"];
 const romanLettersRegex = new RegExp("[^" + romanTable.join("") + "]"); //Regex: /[^IVXLCDM]/
 const invalidInput = "not a valid input…";
 
-// Manages form and displays result
+// Manages form event listener
 var displayResult = document.getElementById("resultContainer");
 document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
   const userInput = document.getElementById("userInput").value.toUpperCase();
-  
-  //We use the right converter function depending on the input
+
+  // Only roman letters -> convertToArabic()
   if (userInput.search(romanLettersRegex) == -1) {
     displayResult.textContent = convertToArabic(userInput);
-  } else if (
+  }
+  // Only figures -> convertToRoman()
+  else if (
     userInput.search(/[^\d]/) == -1 &&
     userInput > 0 &&
     userInput < 4000
@@ -25,29 +28,25 @@ document.querySelector("form").addEventListener("submit", function (e) {
   } else {
     displayResult.textContent = invalidInput;
   }
-
-  e.preventDefault();
 });
-
 
 /**
  * Converts an arabic numeral into a roman numeral.
  * Each figure is converted starting from units and using a portion of the romanTable.
  *
  * @function convertToRoman
- * @param {string} arabicNumeral - The arabic numeral to convert 
- * @returns {string} An roman numeral
+ * @param {string} arabicNumeral - The arabic numeral to convert
+ * @returns {string} A roman numeral (romanNumeral)
  */
 function convertToRoman(arabicNumeral) {
   const arabicFigures = arabicNumeral.split("").reverse();
-  let romanNumeral = ""; //Variable which will be rendered
+  let romanNumeral = "";
 
   arabicFigures.forEach(function (arabicFigure, i) {
-    
     // Gets the right figures from romanTable using the current arabic figure's place in the number (unit, tens, hundred, thousand).
-    // ex: Units get ["I", "V", "X"], tens get ["X", "L", "C"]...
+    // eg: Units get ["I", "V", "X"], tens get ["X", "L", "C"]...
     let romanTableExtract = romanTable.slice(i * 2, i * 2 + 3);
-    
+
     let romanFigure;
     if (arabicFigure >= 0 && arabicFigure <= 3) {
       romanFigure = romanTableExtract[0].repeat(arabicFigure);
@@ -55,7 +54,7 @@ function convertToRoman(arabicNumeral) {
       romanFigure = romanTableExtract[0] + romanTableExtract[1];
     } else if (arabicFigure >= 5 && arabicFigure <= 8) {
       romanFigure =
-        romanTableExtract[1] + romanTableExtract[0].repeat(arabicFigure - 5); // ex: V + II
+        romanTableExtract[1] + romanTableExtract[0].repeat(arabicFigure - 5); // eg: VIII = 5 + (1 *(8 - 5))
     } else {
       romanFigure = romanTableExtract[0] + romanTableExtract[2]; //9
     }
@@ -66,22 +65,20 @@ function convertToRoman(arabicNumeral) {
   return romanNumeral;
 }
 
-
 /**
- * Convert a roman numeral into an arabic numeral by analysing each figure,
- * and adding (ex: "XV" = 10 + 5) or substracting (ex: "IV" = 5 - 1) accordingly to the returned arabic numeral.
- * Additionnaly, check if the roman numeral is invalid and return the invalid message in that case.
+ * Converts a roman numeral into an arabic numeral.
+ * Analyses each roman figure from the right.
+ * Adds it (eg: "XV" = 10 + 5) or substracts it (eg: "IV" = 5 - 1) to the arabicNumeral to be returned.
  *
- * @constructor
- * @param {string} The roman numeral to convert
- * @returns {number | string} An arabic numeral or the invalidInput string.
+ * @function convertToArabic
+ * @param {string} romanNumeral - The roman numeral to convert
+ * @returns {number | string} An arabic numeral (arabicNumeral) or the invalid message (invalidInput)
  */
-// We want to go from unit to thousands
-// We check all conditions
-// We add or substract the currentFigure to the total arabicNumeral
 function convertToArabic(romanNumeral) {
   const romanFigures = romanNumeral.split("").reverse();
-  let arabicNumeral = 0; //Variable which will be rendered
+  let arabicNumeral = 0;
+
+  //Variables to compare
   let lastFigure = 0;
   let beforeLastFigure = 0;
   let counterEqual = 0; //Used to count identical successive figures
@@ -91,40 +88,53 @@ function convertToArabic(romanNumeral) {
     let tableIndex = romanTable.indexOf(romanFigures[i]);
     let currentFigure = arabicTable[tableIndex];
 
-    // Checking if the romanNumeral input is invalid
-    if (
-      (currentFigure == lastFigure &&
-        Number.isInteger(tableIndex / 2) == false) ||
-      (currentFigure == lastFigure && counterEqual >= 2) ||
-      (currentFigure == lastFigure && currentFigure < beforeLastFigure) ||
-      (currentFigure < lastFigure && currentFigure <= beforeLastFigure) ||
-      (currentFigure < lastFigure &&
-        currentFigure * 5 != lastFigure &&
-        currentFigure * 10 != lastFigure)      
-    ) {
+    if (isInvalidRomanNum(currentFigure, lastFigure, beforeLastFigure)) {
       return invalidInput;
-    }
-
-    //When the input is valid
-    else {
+    } else {
       if (currentFigure > lastFigure) {
         counterEqual = 0;
-        arabicNumeral += currentFigure;
+        arabicNumeral += currentFigure; //eg: XV
       } else if (currentFigure == lastFigure) {
         counterEqual++;
-        arabicNumeral += currentFigure;
-      } else {
-        arabicNumeral -= currentFigure;
+        arabicNumeral += currentFigure; //eg: XX
+      } else if (currentFigure < lastFigure) {
+        arabicNumeral -= currentFigure; //eg: IX
       }
 
+      if (arabicNumeral > 3999) return invalidInput;
+
+      // Updates variables for next iteration
       beforeLastFigure = lastFigure;
       lastFigure = currentFigure;
       i++;
     }
-    
-    if (arabicNumeral > 3999) {
-      return invalidInput;
-    }
   }
   return arabicNumeral;
+}
+
+/**
+ * Returns true if the roman numeral is invalid (ie: The roman figures are not in the right order).
+ * Checks all cases by comparing the figures.
+ * eg: VV, IIII, IIV, IVI, IVX, IC
+ *
+ * @function isInvalidRomanNum
+ * @param {number} currentFigure - Currently checken arabic figure
+ * @param {number} lastFigure - Last checken arabic figure
+ * @param {number} beforeLastFigure - Before last checken arabic figure
+ * @returns {boolean} True if invalid
+ */
+function isInvalidRomanNum(currentFigure, lastFigure, beforeLastFigure) {
+  if (
+    (currentFigure == lastFigure && !Number.isInteger(tableIndex / 2)) ||
+    (currentFigure == lastFigure && counterEqual >= 2) ||
+    (currentFigure == lastFigure && currentFigure < beforeLastFigure) ||
+    (currentFigure < lastFigure && currentFigure <= beforeLastFigure) ||
+    (currentFigure < lastFigure &&
+      currentFigure * 5 != lastFigure &&
+      currentFigure * 10 != lastFigure)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
